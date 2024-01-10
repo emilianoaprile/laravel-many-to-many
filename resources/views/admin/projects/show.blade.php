1b8938609ed5a90f066de674c72b071e08207473
EmilianoAprile <emilianoaprile77@icloud.com>
2024-01-10 17:11:27 +0100

    ADDED - technologies printed into show file

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -12,6 +12,12 @@
         </p>
         @endif
         <p>{{ $project->slug }}</p>
+        <ul class="d-flex gap-2 ps-0">
+          <strong>Technologies used:</strong>
+          @foreach ($project->technologies as $technology)
+            <li class="badge rounded-pill text-bg-primary">{{ $technology->name }}</li>
+          @endforeach 
+        </ul>
         <p>{{ $project->created_at->format('d/m/Y') }}</p>
       </div>
     </section>
